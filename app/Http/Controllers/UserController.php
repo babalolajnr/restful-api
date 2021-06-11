f76a593bbc4cead2e92bd5ff001dc1803d91bf99
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-06-11 22:18:17 +0100

    create User controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Update user
+     *
+     * @param  Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request)
+    {
+
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'max:255']
+        ]);
+
+
+        $request->user()->update($data);
+
+        return response()->json(['success' => 'User details updated successfully!', $data], 200);
+    }
+
+    /**
+     * Show user details
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show()
+    {
+        $user = auth()->user();
+        return response()->json([$user], 200);
+    }
+
+    public function destroy()
+    {
+        $user = User::find(auth()->user()->id);
+
+        //Revoke tokens
+        $user->tokens()->delete();
+
+        $user->delete();
+
+        return response()->json(['success' => 'User deleted successfully!'], 204);
+    }
+}
